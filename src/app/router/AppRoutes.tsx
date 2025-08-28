1cd993929bd8497c044fbe9c182d8b97fc2af8d2
CunhaBSb <marcosdocunha@gmail.com>
2025-08-27 23:25:23 -0300

    teste1

--- src/app/router/AppRoutes.tsx
+++ src/app/router/AppRoutes.tsx
@@ -1,7 +1,6 @@
 import { Routes, Route } from "react-router-dom";
 import { Home } from "@/pages/home";
 import { Reveillon } from "@/pages/reveillon";
-import { Pyrotechnics } from "@/pages/pyrotechnics";
 import NotFound from "@/shared/pages/NotFound";
 
 export default function AppRoutes() {
@@ -9,8 +8,6 @@ export default function AppRoutes() {
     <Routes>
       <Route path="/" element={<Home />} />
       <Route path="/reveillon" element={<Reveillon />} />
-      <Route path="/shows" element={<Pyrotechnics />} />
-      <Route path="/pirotecnia" element={<Pyrotechnics />} />
       <Route path="*" element={<NotFound />} />
     </Routes>
   );
--- src/pages/home/index.ts
+++ src/pages/home/index.ts
@@ -4,7 +4,7 @@ export { default as Home } from './HomePage';
 // Desktop specific exports
 export { default as HomeDesktop } from './desktop/Home';
 export { default as Hero } from './desktop/components/Hero';
-export { default as Services } from './desktop/components/shows';
+export { default as Services } from './desktop/components/Services';
 
 // Mobile specific exports
 export { default as HomeMobile } from './mobile/Home';
